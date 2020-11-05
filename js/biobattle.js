8cd15d54e15f62312d653ef67b9acadcd8ddc26e
Jeremy Hopkins <hopkinsje09@students.ecu.edu>
2020-11-05 16:03:11 -0500

    supports title screen and audio options
    Added title screen
    sound effects for fire and hits and background

--- js/biobattle.js
+++ js/biobattle.js
@@ -12,6 +12,23 @@ canvas.width = 820;
 canvas.height = 760;
 document.body.appendChild(canvas);
 
+// Title Screen
+var titleReady = false;
+var firstLoad = false;
+var titleImage = new Image();
+titleImage.onload = function () {
+	titleReady = true;
+	firstLoad = true;
+	document.body.style.backgroundColor = "black";
+}
+titleImage.src = "images/background.png";
+
+// Audio Clips
+var audioTitle = new Audio("audio/title.mp3");
+var audioTitleReady = true;
+var audioRNA = new Audio("audio/fire.mp3");
+var audioHIT = new Audio("audio/hit.mp3");
+
 // Background image
 var bgReady = false;
 var bgImage = new Image();
@@ -70,6 +87,7 @@ var cheats = {
 	sarsLaunchIndicator: false,
 	hud: false,
 	nextLaunch: false,
+	mute: false,
 }
 
 // Position the cannon of the Jonas Salk
@@ -128,7 +146,7 @@ var keysDown = {};
 addEventListener("keypress", function(e) {
 	if (debug.level == "verbose") console.log("keypress: ", e.key);
 	if (e.key == " ") {
-		if (score.gameover == true) {
+		if (score.gameover == true || firstLoad) {
 			newGame();
 		} else {
 			score.paused = !score.paused;
@@ -176,6 +194,17 @@ addEventListener("keypress", function(e) {
 		cheats.nextTarget = (Math.abs(cheats.nextTarget - 5));
 		console.log("Next SARS Target Location set to :" + cheats.nextTarget);
 	}
+	if (e.key ==  "q") {
+		console.log ("Quitting game and starting over");
+		if (score.paused) {
+			score.paused = !score.paused;
+			score.gameover = !score.gameover;
+		}
+	}
+	if (e.key == "m") {
+		cheats.mute = !cheats.mute;
+		if (debug.level == "info") console.log("cheats.mute: ", cheats.mute);
+	}
 }, false);
 
 addEventListener("mousemove", function (e) {
@@ -188,29 +217,38 @@ addEventListener("mousemove", function (e) {
 
 addEventListener("contextmenu", function (e) {
 	e.preventDefault();
-	score.paused = !score.paused;
-	if (debug.level == "info") console.log("Game Paused: ", score.paused);
+	if (!firstLoad) {
+		score.paused = !score.paused;
+		if (debug.level == "info") console.log("Game Paused: ", score.paused);
+	}
 }, false);
 
 addEventListener("click", function (e) {
-	if (score.levelup == true) {
-		score.levelup = false;
+	if (firstLoad) {
+		firstLoad = !firstLoad;
+		audioTitleReady = !audioTitleReady;
+		reset();
 	} else {
-		if (debug.level == "info") console.log("Shot fired towards: ", e.clientX, e.clientY);
-		// the - 21 is an adjustment to move the image to the middle of the crosshairs
-		// otherwise the top left corner of the image is placed at the bottom right corner of the crosshair
-		rna.Xtarget = e.clientX - 21;
-		rna.Ytarget = e.clientY - 21;
-		// keep the cursor target on the battleground
-		if (rna.Xtarget < 100) {
-			rna.Xtarget = 100;
-		} else if (rna.Xtarget > 735) {
-			rna.Xtarget = 735;
-		}
-		if (rna.Ytarget < 60) {
-			rna.Ytarget = 60;
-		} else if (rna.Ytarget > 475) {
-			rna.Ytarget = jonas.y - 13;
+		if (score.levelup) {
+			score.levelup = !score.levelup;
+		} else {
+			if (debug.level == "info") console.log("Shot fired towards: ", e.clientX, e.clientY);
+			// the - 21 is an adjustment to move the image to the middle of the crosshairs
+			// otherwise the top left corner of the image is placed at the bottom right corner of the crosshair
+			if (!cheats.mute) {audioRNA.play() };
+			rna.Xtarget = e.clientX - 21;
+			rna.Ytarget = e.clientY - 21;
+			// keep the cursor target on the battleground
+			if (rna.Xtarget < 100) {
+				rna.Xtarget = 100;
+			} else if (rna.Xtarget > 735) {
+				rna.Xtarget = 735;
+			}
+			if (rna.Ytarget < 60) {
+				rna.Ytarget = 60;
+			} else if (rna.Ytarget > 475) {
+				rna.Ytarget = jonas.y - 13;
+			}
 		}
 	}
 }, false);
@@ -297,6 +335,7 @@ var update = function (modifier) {
 			&& rna.y <= (sars.y + 25)
 			&& sars.y <= (rna.y + 25)
 		) {
+			if (!cheats.mute) {audioHIT.play() };
 			sarsDestroyed++;
 			if (sarsDestroyed % 2 == 0) {
 				sars.speed ++;
@@ -328,169 +367,187 @@ var update = function (modifier) {
 
 // Draw everything
 var render = function () {
-	if (strikeReady && strikeActive) {
+	if (firstLoad) {
+		if (titleReady && audioTitleReady) {
+			ctx.drawImage(titleImage, 0,0);
+			//document.body.style.backgroundColor = initial;
+			if (!cheats.mute) {audioTitle.play() } else { audioTitle.pause() };
+			// Game Logo 
+			var title_gradient = ctx.createLinearGradient(235, 325, 535, 325);
+			title_gradient.addColorStop(0, "red");
+			title_gradient.addColorStop(1, "#d0fa0d");
+			ctx.fillStyle = title_gradient;
+			ctx.font = "92px Impact";
+			ctx.textAlign = "left";
+			ctx.fillText("BioBattle", 235, 330)
+			ctx.font = "16px Impact";
+			ctx.fillText("Click for new game", 335, 355);
+
+
+		}
+	} else if (strikeReady && strikeActive) {
 		ctx.drawImage(strikeImage, 0, 0);
 		strikeActive = false;
 	} else if (bgReady) {
+		audioTitle.pause();
+		audioTitleReady = false;
 		ctx.drawImage(bgImage, 0, 0);
-	}
-	if (jonasReady) {
-		ctx.drawImage(jonasImage, jonas.x, jonas.y);
-	}
-	if (score.gameover) {
+		if (jonasReady) {
+			ctx.drawImage(jonasImage, jonas.x, jonas.y);
+		}
+		if (score.gameover) {
+			ctx.font = "92px Impact";
+			ctx.textAlign = "left";
+			ctx.fillText("GAME OVER", 210, 325)
+			ctx.font = "16px Impact";
+			ctx.fillText("Press spacebar for new game", 330, 350);
+		} else {
+			if (score.paused) {
+				ctx.font = "92px Impact";
+				ctx.textAlign = "center";
+				ctx.fillText("PAUSED", 410, 325);	
+				ctx.font = "16px Impact";
+				ctx.fillText("Right-click or press spacebar to resume", 410, 350);
+			} else if (score.levelup) {
+				ctx.font = "92px Impact";
+				ctx.textAlign = "center";
+				ctx.fillText("LEVEL " + score.level, 410, 325);
+				ctx.font = "16px Impact";
+				ctx.fillText("Click to begin level " + score.level, 410, 350);	
+			} else if (rnaReady && sarsReady) {
+				ctx.drawImage(sarsImage, sars.x, sars.y);
+				ctx.drawImage(rnaImage, rna.x, rna.y);
+			}
+		}
+		if (cheats.menu) {
+			ctx.font = "14px Arial Black";
+			ctx.fillStyle = "white";
+			ctx.textAlign = "center";
+			var start = 225;
+			ctx.fillText("Cheat/Shortcut Menu", 410, start);
+			ctx.fillText("--------------------------------------", 410, start+=15);
+			ctx.fillText("space/right-click - Pause/Resume game", 410,start+=15);
+			ctx.fillText("q - Quit game (must be paused first)", 410,start+=15);
+			ctx.fillText("m - Toggle sound effects", 410, start+=15);
+			ctx.fillText("h - Toggle HUD (Heads Up Display)", 410, start+=15);
+			ctx.fillText("T - Toggle SARS Spike Target Indicator", 410, start+=15);
+			ctx.fillText("t - Toggle all potential SARS Spike Target Indicators", 410, start+=15);
+			ctx.fillText("O - Toggle SARS Spike Launch Origin Indicator", 410, start+=15);
+			ctx.fillText("o - Toggle all SARS Spike Origin Indicators", 410, start+=15);
+			ctx.fillText("s - Increase SARS speed", 410, start+=15);
+			ctx.fillText("S - Decrease SARS speed", 410, start+=15);
+			ctx.fillText("1-5 - Press 1 through 5 to set next SARS launch position", 410, start+=15);
+			ctx.fillText("6-0 - Press 6 through 0 to set next SARS target position", 410, start+=15);
+			ctx.fillText("? - Toggle cheat menu", 410, start+=15);
+		}
+
+		if (cheats.hud) {
+			ctx.font = "14px Arial Black";
+			ctx.fillStyle = "white";
+			ctx.textAlign = "center";
+			var sarsDistance = trajectory({x: sars.x,y: sars.y},{x: sars.Xtarget, y: sars.Ytarget});
+			var rnaDistance = trajectory({x: rna.x,y: rna.y},{x: rna.Xtarget, y: rna.Ytarget});
+			var sarsHudStart = rnaHudStart = 75;
+			ctx.fillText("SARS Spike Speed: " + sars.pps.toFixed(2) + " pps", 230, sarsHudStart);
+			ctx.fillText("SARS Distance to Target: " + sarsDistance.toFixed(2) + " pixels", 230, sarsHudStart+=15);
+			ctx.fillText("SARS Time to Impact: " + (sarsDistance/sars.pps).toFixed(1) + " seconds", 230, sarsHudStart+=15);
+			ctx.fillText("RNA Speed: " + rna.pps.toFixed(2) + " pps", 610, rnaHudStart);
+			ctx.fillText("RNA Distance to Target: " + rnaDistance.toFixed(2) + " pixels", 610, rnaHudStart+=15);
+			ctx.fillText("RNA Time to Target: " + (rnaDistance/rna.pps).toFixed(1) + " seconds", 610, rnaHudStart+=15);
+			ctx.textAlign = "left";
+			ctx.fillText(rna.Xcrosshair + "," + rna.Ycrosshair, rna.Xcrosshair + 21, rna.Ycrosshair);
+			ctx.fillText(Math.floor(rna.x) + "," + Math.floor(rna.y), rna.x + 21, rna.y);
+			ctx.fillStyle = "red";
+			ctx.fillText("S", sars.x, sars.y);
+			ctx.fillText("X", sars.Xtarget, sars.Ytarget);
+			ctx.beginPath();
+			ctx.strokeStyle = "green";
+			ctx.lineWidth = 2;
+			ctx.moveTo(jonas.x+13, jonas.y);
+			ctx.lineTo(rna.Xcrosshair+13, rna.Ycrosshair+18 );
+			ctx.stroke();
+			ctx.beginPath();
+			ctx.strokeStyle = "red";
+			ctx.lineWidth = 2;
+			ctx.moveTo(sars.x, sars.y);
+			ctx.lineTo(sars.Xtarget, sars.Ytarget);
+			ctx.stroke();
+			ctx.beginPath();
+
+		
+		}
+
+		// Target Indicators
+		if ( cheats.targetIndicators == true ) {
+			ctx.fillStyle = "red";
+			ctx.textAlign = "center";
+			ctx.font = "16px Arial Black";
+			for (let index = 0; index < (sars.XtargetSites).length; index++) {
+				ctx.fillText("x", (sars.XtargetSites)[index], (sars.YtargetSites)[index]+13);			
+			}
+		}
+		if ( cheats.sarsTargetIndicator == true ) {
+			ctx.fillStyle = "red";
+			ctx.textAlign = "center";
+			ctx.font = "16px Arial Black";
+			ctx.fillText("X", sars.Xtarget, sars.Ytarget+13);			
+		}
+
+		// Origin Indicators
+		if ( cheats.originIndicators == true ) {
+			ctx.fillStyle = "white";
+			ctx.textAlign = "center";
+			ctx.font = "16px Arial Black";
+			for (let index = 0; index < (sars.XlaunchSites).length; index++) {
+				ctx.fillText("o", (sars.XlaunchSites)[index], (sars.YlaunchSites)[index]+13);			
+			}
+		}
+		if ( cheats.sarsLaunchIndicator == true ) {
+			ctx.fillStyle = "white";
+			ctx.textAlign = "center";
+			ctx.font = "16px Arial Black";
+			ctx.fillText("O", (sars.XlaunchSites)[sars.launchPosition], (sars.YlaunchSites)[sars.launchPosition]+13);			
+		}
+
+
+		// Game Logo 
+		var my_gradient = ctx.createLinearGradient(50, 700, 350, 700);
+		my_gradient.addColorStop(0, "red");
+		my_gradient.addColorStop(1, "#d0fa0d");
+		ctx.fillStyle = my_gradient;
 		ctx.font = "92px Impact";
 		ctx.textAlign = "left";
-		ctx.fillText("GAME OVER", 210, 325)
-		ctx.font = "16px Impact";
-		ctx.fillText("Press spacebar for new game", 330, 350);
-	} else {
-		if (score.paused) {
-			ctx.font = "92px Impact";
-			ctx.textAlign = "center";
-			ctx.fillText("PAUSED", 410, 325);	
-			ctx.font = "16px Impact";
-			ctx.fillText("Right-click or press spacebar to resume", 410, 350);
-		} else if (score.levelup) {
-			ctx.font = "92px Impact";
-			ctx.textAlign = "center";
-			ctx.fillText("LEVEL " + score.level, 410, 325);
-			ctx.font = "16px Impact";
-			ctx.fillText("Click to begin level " + score.level, 410, 350);	
-		} else if (rnaReady && sarsReady) {
-			ctx.drawImage(sarsImage, sars.x, sars.y);
-			ctx.drawImage(rnaImage, rna.x, rna.y);
-		}
-	}
-	if (cheats.menu) {
-		ctx.font = "14px Arial Black";
-		ctx.fillStyle = "white";
-		ctx.textAlign = "center";
-		var start = 225;
-		ctx.fillText("Cheat/Shortcut Menu", 410, start);
-		ctx.fillText("--------------------------------------", 410, start+=15);
-		ctx.fillText("space/right-click - Pause/Resume game", 410,start+=15);
-		ctx.fillText("h - Toggle HUD (Heads Up Display)", 410, start+=15);
-		ctx.fillText("T - Toggle SARS Spike Target Indicator", 410, start+=15);
-		ctx.fillText("t - Toggle all potential SARS Spike Target Indicators", 410, start+=15);
-		ctx.fillText("O - Toggle SARS Spike Launch Origin Indicator", 410, start+=15);
-		ctx.fillText("o - Toggle all SARS Spike Origin Indicators", 410, start+=15);
-		ctx.fillText("s - Increase SARS speed", 410, start+=15);
-		ctx.fillText("S - Decrease SARS speed", 410, start+=15);
-		ctx.fillText("1-5 - Press 1 through 5 to set next SARS launch position", 410, start+=15);
-		ctx.fillText("6-0 - Press 6 through 0 to set next SARS target position", 410, start+=15);
-		ctx.fillText("? - Toggle cheat menu", 410, start+=15);
-	}
+		ctx.fillText("BioBattle", 50, 700);
 
-	if (cheats.hud) {
-		ctx.font = "14px Arial Black";
+		// Text properties
 		ctx.fillStyle = "white";
-		ctx.textAlign = "center";
-		var sarsDistance = trajectory({x: sars.x,y: sars.y},{x: sars.Xtarget, y: sars.Ytarget});
-		var rnaDistance = trajectory({x: rna.x,y: rna.y},{x: rna.Xtarget, y: rna.Ytarget});
-		var sarsHudStart = rnaHudStart = 75;
-		ctx.fillText("SARS Spike Speed: " + sars.pps.toFixed(2) + " pps", 230, sarsHudStart);
-		ctx.fillText("SARS Distance to Target: " + sarsDistance.toFixed(2) + " pixels", 230, sarsHudStart+=15);
-		ctx.fillText("SARS Time to Impact: " + (sarsDistance/sars.pps).toFixed(1) + " seconds", 230, sarsHudStart+=15);
-		ctx.fillText("RNA Speed: " + rna.pps.toFixed(2) + " pps", 610, rnaHudStart);
-		ctx.fillText("RNA Distance to Target: " + rnaDistance.toFixed(2) + " pixels", 610, rnaHudStart+=15);
-		ctx.fillText("RNA Time to Target: " + (rnaDistance/rna.pps).toFixed(1) + " seconds", 610, rnaHudStart+=15);
+		ctx.font = "16px Arial Black";
+
+		// Level Indicator
 		ctx.textAlign = "left";
-		ctx.fillText(rna.Xcrosshair + "," + rna.Ycrosshair, rna.Xcrosshair + 21, rna.Ycrosshair);
-		ctx.fillText(Math.floor(rna.x) + "," + Math.floor(rna.y), rna.x + 21, rna.y);
-		ctx.fillStyle = "red";
-		ctx.fillText("S", sars.x, sars.y);
-		ctx.fillText("X", sars.Xtarget, sars.Ytarget);
+		ctx.fillText("Level ", 175, 725);
+		ctx.fillText(score.level, 225, 725);
+
+		// Scoreboard
 		ctx.beginPath();
-		ctx.strokeStyle = "green";
-		ctx.lineWidth = 2;
-		ctx.moveTo(jonas.x+13, jonas.y);
-		ctx.lineTo(rna.Xcrosshair+13, rna.Ycrosshair+18 );
+		ctx.textAlign = "left";
+		ctx.fillText("Spikes Destroyed:", 475, 625);
+		ctx.fillText("Lives saved:", 475, 650);
+		ctx.strokeStyle = "white";
+		ctx.lineWidth = 5;
+		ctx.moveTo(475, 670);
+		ctx.lineTo(720, 670);
+		ctx.fillText("Spikes Delivered:", 475, 700);
+		ctx.fillText("Lives lost:", 475, 725);
+		ctx.textAlign = "right";
+		ctx.fillStyle = "#d0fa0d";
+		ctx.fillText(sarsDestroyed, 720, 625);
+		ctx.fillText(score.value, 720, 650);
+		ctx.fillStyle = "red";
+		ctx.fillText(score.sarsDelivered, 720, 700);
+		ctx.fillText(score.sars, 720, 725);
 		ctx.stroke();
-		ctx.beginPath();
-		ctx.strokeStyle = "red";
-		ctx.lineWidth = 2;
-		ctx.moveTo(sars.x, sars.y);
-		ctx.lineTo(sars.Xtarget, sars.Ytarget);
-		ctx.stroke();
-		ctx.beginPath();
-
-	
 	}
-
-	// Target Indicators
-	if ( cheats.targetIndicators == true ) {
-		ctx.fillStyle = "red";
-		ctx.textAlign = "center";
-		ctx.font = "16px Arial Black";
-		for (let index = 0; index < (sars.XtargetSites).length; index++) {
-			ctx.fillText("x", (sars.XtargetSites)[index], (sars.YtargetSites)[index]+13);			
-		}
-	}
-	if ( cheats.sarsTargetIndicator == true ) {
-		ctx.fillStyle = "red";
-		ctx.textAlign = "center";
-		ctx.font = "16px Arial Black";
-		ctx.fillText("X", sars.Xtarget, sars.Ytarget+13);			
-	}
-
-	// Origin Indicators
-	if ( cheats.originIndicators == true ) {
-		ctx.fillStyle = "white";
-		ctx.textAlign = "center";
-		ctx.font = "16px Arial Black";
-		for (let index = 0; index < (sars.XlaunchSites).length; index++) {
-			ctx.fillText("o", (sars.XlaunchSites)[index], (sars.YlaunchSites)[index]+13);			
-		}
-	}
-	if ( cheats.sarsLaunchIndicator == true ) {
-		ctx.fillStyle = "white";
-		ctx.textAlign = "center";
-		ctx.font = "16px Arial Black";
-		ctx.fillText("O", (sars.XlaunchSites)[sars.launchPosition], (sars.YlaunchSites)[sars.launchPosition]+13);			
-	}
-
-
-	// Title
-//	ctx.fillStyle = "red";
-	var my_gradient = ctx.createLinearGradient(50, 700, 350, 700);
-	my_gradient.addColorStop(0, "red");
-	my_gradient.addColorStop(1, "#d0fa0d");
-	ctx.fillStyle = my_gradient;
-
-	// Title
-	ctx.font = "92px Impact";
-	ctx.textAlign = "left";
-	ctx.fillText("BioBattle", 50, 700);
-
-	// Text properties
-	ctx.fillStyle = "white";
-	ctx.font = "16px Arial Black";
-
-	// Level Indicator
-	ctx.textAlign = "left";
-	ctx.fillText("Level ", 175, 725);
-	ctx.fillText(score.level, 225, 725);
-
-	// Scoreboard
-	ctx.beginPath();
-	ctx.textAlign = "left";
-	ctx.fillText("Spikes Destroyed:", 475, 625);
-	ctx.fillText("Lives saved:", 475, 650);
-	ctx.strokeStyle = "white";
-	ctx.lineWidth = 5;
-	ctx.moveTo(475, 670);
-	ctx.lineTo(720, 670);
-	ctx.fillText("Spikes Delivered:", 475, 700);
-	ctx.fillText("Lives lost:", 475, 725);
-	ctx.textAlign = "right";
-	ctx.fillStyle = "#d0fa0d";
-	ctx.fillText(sarsDestroyed, 720, 625);
-	ctx.fillText(score.value, 720, 650);
-	ctx.fillStyle = "red";
-	ctx.fillText(score.sarsDelivered, 720, 700);
-	ctx.fillText(score.sars, 720, 725);
-	ctx.stroke();
-
 };
 
 var trajectory = function(start,end) {
